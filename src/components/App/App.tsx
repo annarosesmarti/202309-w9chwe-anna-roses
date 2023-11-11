c4ac2ccd9b2d132736ea2e6024406b25a6126148
Anna Roses <anna.roses.marti@gmail.com>
2023-11-11 14:35:57 +0100

    Fix App links to invalid components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,8 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Header from "../Header/Header";
 
 const App = (): React.ReactElement => {
   return (
     <>
-      <Header titleText="Hogwarts" subtitleText=" The social network ·" />
       <Routes>
         <Route path="/" element={<Navigate to={"/home"} />} />
       </Routes>
