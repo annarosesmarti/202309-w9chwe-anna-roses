dfff9ebbb48dfa025c5dba6a3b158fba1bf244f0
Anna Roses <anna.roses.marti@gmail.com>
2023-11-11 21:52:27 +0100

    Add peopleReducer test

---
+++ src/store/features/people/peopleReducer.test.ts
@@ -0,0 +1,22 @@
+import mockPeople from "../../../mocks/mockData";
+import { PeopleStructure } from "../../types";
+import { loadPeopleActionCreator, peopleReducer } from "./peopleSlice";
+
+describe("Given a PeopleReducer reducer", () => {
+  describe("When it receives an empty list and a people list", () => {
+    test("Then it should return a List of 15 people in it", () => {
+      const currentListState: PeopleStructure = {
+        people: [],
+      };
+
+      const peopleList = mockPeople;
+
+      const newPeopleState = peopleReducer(
+        currentListState,
+        loadPeopleActionCreator(peopleList),
+      );
+
+      expect(newPeopleState.people).toStrictEqual(peopleList);
+    });
+  });
+});
