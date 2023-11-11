5034675f745fc8a8eee20cd4db744f97f7a538f8
Anna Roses <anna.roses.marti@gmail.com>
2023-11-11 15:29:12 +0100

    Add HomePage files to a HomePage folder

---
+++ src/Pages/HomePage/HomePage.test.tsx
@@ -0,0 +1,20 @@
+import { screen } from "@testing-library/react";
+import customRender from "../../testUtils/customRender";
+import HomePage from "./HomePage";
+
+describe("Given a HomePage component", () => {
+  describe("When it is rendered", () => {
+    test("Then it shouls show the title 'Create your network'", () => {
+      const expectedTitle = "Create your network";
+
+      customRender(<HomePage />);
+
+      const title = screen.getByRole("heading", {
+        hidden: true,
+        name: expectedTitle,
+      });
+
+      expect(title).toBeInTheDocument();
+    });
+  });
+});
