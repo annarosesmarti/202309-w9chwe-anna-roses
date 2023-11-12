24196f6e2905e5e79bff2e7f3a9367c8152828e1
Anna Roses <anna.roses.marti@gmail.com>
2023-11-12 16:35:11 +0100

    Add customRenderWithProviders and fix describe

--- src/Pages/HomePage/HomePage.test.tsx
+++ src/Pages/HomePage/HomePage.test.tsx
@@ -1,16 +1,16 @@
 import { screen } from "@testing-library/react";
 import HomePage from "./HomePage";
-import { customRenderStore } from "../../testUtils/customRender";
+import { customRenderWithProviders } from "../../testUtils/customRender";
+import mockPeople from "../../mocks/mockData";
 
 describe("Given a HomePage component", () => {
-  describe("When it is rendered", () => {
+  describe("When it is rendered and receives a list of two people", () => {
     test("Then it should show the title 'Create your network'", () => {
       const expectedTitle = "Create your network";
+      const mockData = mockPeople;
 
-      customRenderStore(<HomePage />);
-
+      customRenderWithProviders(<HomePage />, mockData);
       const title = screen.getByRole("heading", {
-        hidden: true,
         name: expectedTitle,
       });
 
