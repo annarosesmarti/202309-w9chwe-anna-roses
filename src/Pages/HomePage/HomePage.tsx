d8114859133a340683d99bb7dbd221fe1a2611e4
Anna Roses <anna.roses.marti@gmail.com>
2023-11-12 01:25:07 +0100

    Add dispatcher to HomePage

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import HomePageStyled from "./HomePageStyled";
 import PeopleList from "../../components/PeopleList/PeopleList";
+import usePeopleApi from "../../Hooks/usePeopleApi";
+import { loadPeopleActionCreator } from "../../store/features/people/peopleSlice";
 
 const HomePage = (): React.ReactElement => {
+  const { getPeople } = usePeopleApi();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      const currentPeople = await getPeople();
+      dispatch(loadPeopleActionCreator(currentPeople));
+    })();
+  }, [dispatch, getPeople]);
+
   return (
     <>
       <HomePageStyled className="header">
