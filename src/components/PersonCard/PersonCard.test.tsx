2fc8950ce77d99b29f1b1f100837ab1ea3db88d6
Anna Roses <anna.roses.marti@gmail.com>
2023-11-12 16:32:54 +0100

    Fix test description

--- src/components/PersonCard/PersonCard.test.tsx
+++ src/components/PersonCard/PersonCard.test.tsx
@@ -1,16 +1,15 @@
 import { screen } from "@testing-library/react";
-import mockPeople from "../../mocks/mockData";
 import PersonCard from "./PersonCard";
 import { customRender } from "../../testUtils/customRender";
+import mockPeople from "../../mocks/mockData";
 
 describe("Given a PersonCard compomnent", () => {
-  describe("When it is rendered", () => {
+  describe("When it is rendered and receives a person and a key number 0", () => {
     test("Then it should show 'Harry' in the alternative text of the image", () => {
       const mockPerson = mockPeople[0];
       const expectedText = mockPerson.name;
 
       customRender(<PersonCard key={0} person={mockPerson} />);
-
       const text = screen.getByRole("img", {
         name: expectedText,
       });
