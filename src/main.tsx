a6c6770ff2618a19b406427d57b12ecf6f38a507
Anna Roses <anna.roses.marti@gmail.com>
2023-11-12 01:23:01 +0100

    Add provider to main

--- src/main.tsx
+++ src/main.tsx
@@ -8,14 +8,18 @@ import { ThemeProvider } from "styled-components";
 import mainTheme from "./styles/MainTheme";
 import App from "./components/App/App";
 import GlobalStyles from "./styles/GlobalStyles";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={mainTheme}>
-        <GlobalStyles />
-        <App />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyles />
+          <App />
+        </ThemeProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
